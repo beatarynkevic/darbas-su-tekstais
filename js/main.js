90d1ce91da28d325e28115075032d3853f9c07f3
beatarynkevic <beatainn@gmail.com>
2020-11-30 12:55:28 +0200

    Initial code

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// DARBAS SU TEKSTAIS
+
+// Turime teksta (gali buti keli paragrafai).
+// Jame yra sakiniu.
+
+// Isspausdinti lentele:
+// - kiek yra paragrafu
+// - kiek yra sakiniu
+// - kiek yra zodziu
+// - kiek yra tekstiniu simboliu (isskaitant tarpus)
+// - kiek yra raidziu
+// - kiek yra skaiciu
+// - kiek yra tikriniu zodziu (ivardziai (vardas, pavarde) ir vietovardziai)
